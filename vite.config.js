f4c9bd7971ee62910d110aa0ba1cb4687380e55b
Prachi5791 <patilmg2009@gmail.com>
2024-10-30 21:02:01 +0530

    Commit

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,27 @@
 // })
 // // 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/Gemini_clone_project/', // Set to your specific subdirectory if needed
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
 });
