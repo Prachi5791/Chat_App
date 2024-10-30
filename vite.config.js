9f9f9d655bf839a34af80344e86d494618bb3864
Prachi5791 <patilmg2009@gmail.com>
2024-10-30 22:51:50 +0530

    commit2

--- vite.config.js
+++ vite.config.js
@@ -23,14 +23,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Gemini_clone_project/', // Set to your specific subdirectory if needed
+  base: '/react_chat_application/', // Change this to your repository name
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // This should be where Vite outputs the built files
     rollupOptions: {
       input: {
-        main: 'index.html'
+        main: 'index.html' // Ensure this points to your main HTML file
       }
     }
   }
